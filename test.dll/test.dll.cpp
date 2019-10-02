06f54aa79a9b1a78cb97719fc214292a6db0137d
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2019-10-02 18:46:25 +0900

    Add API class.
    - PImpl

--- test.dll/test.dll.cpp
+++ test.dll/test.dll.cpp
@@ -3,4 +3,49 @@
 
 #include "stdafx.h"
 
+#include "test.dll.h"
 
+namespace test {
+
+class DllClassImpl
+{
+public:
+	DllClassImpl();
+	~DllClassImpl();
+
+	int Test();
+private:
+	int value;
+};
+
+DllClassImpl::DllClassImpl()
+	:value(100)
+{
+}
+
+DllClassImpl::~DllClassImpl()
+{
+}
+
+int DllClassImpl::Test()
+{
+	return value;
+}
+
+
+DllClass::DllClass()
+	:pimpl(new DllClassImpl)
+{
+}
+
+DllClass::~DllClass()
+{
+	delete pimpl;
+}
+
+int DllClass::Test()
+{
+	return pimpl->Test();
+}
+
+}
---
+++ test.dll/test.dll.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifdef TESTDLL_EXPORTS
+#define TESTDLL_API __declspec(dllexport)
+#else
+#define TESTDLL_API __declspec(dllimport)
+#endif
+
+namespace test {
+
+// PImpl(Pointer to implementation)
+class TESTDLL_API DllClass
+{
+public:
+	DllClass();
+	~DllClass();
+
+	int Test();
+private:
+	class DllClassImpl* pimpl;
+};
+
+}
